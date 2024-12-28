58ddb13ec98e7143cccbd3f6bd5bd3acd0763b1e
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2024-12-28 16:49:40 -0600

    Major work on 'units' (needs cleanup).

--- packages/stylts/src/units.ts
+++ packages/stylts/src/units.ts
@@ -12,53 +12,132 @@ let undef: undefined;
 
 export const auto = 'auto';
 
-const unitTypes = [
-  'px',
-  'em',
-  'rem',
-  'vh',
-  'vw',
-  'lh',
-  'rlh',
-  'pt',
-  'pct',
-  '%',
-  ''
+export const cssGlobalValues = [
+  'inherit', 'initial', 'revert', 'revert-layer', 'unset',
 ] as const;
 
-type UnitType = typeof unitTypes[number];
+// CSS unit types listed here:
+// https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Values_and_Units
 
-// type PxUnit = `${number}px`;
-// type PtUnit = `${number}pt`;
-// type EmUnit = `${number}em`;
-// type RemUnit = `${number}rem`;
-// type VhUnit = `${number}vh`;
-// type LhUnit = `${number}lh`;
-// type RlhUnit = `${number}rlh`;
-// type PctUnit = `${number}pct`;
+const unitTypes = {
 
-type UnitValue = number | string | NumericValue | '';
-type UnitValues = UnitValue[] | UnitValue[][];
+  // --- absolute units --- //
+  cm: 'cm', // 'centimeters',
+  in: 'in', // 'inches',
+  mm: 'mm', // 'millimeters',
+  pc: 'pc', //'picas',
+  pt: 'pt', // 'points',
+  px: 'px', // pixels',
+  Q: 'Q', // quarter-millimeters',  // WTF?
 
-export function toUnit(value: UnitValues, unit: (UnitType | never) = ''): string {
+  // --- root element font sizing --- //
+  rcap: 'rcap', // root cap height',
+  rch: 'rch', // root character "0" width',
+  rem: 'rem', // root font size',
+  rex: 'rex', // root font x-height',
+  ric: 'ric', // ??? `Average character advance of a full-width glyph in the root element's font, as represented by the "水" (CJK water ideograph, U+6C34) glyph.`,
+  rlh: 'rlh', // root line height',
+
+  // --- local element font sizing --- //
+  cap: 'cap', // relative cap height',
+  ch: 'ch', // ch', // relative character "0" width',
+  em: 'em', // relative font size',
+  ex: 'ex', // relative font x-height',
+  ic: 'ic', // Average character advance of a full-width glyph in the element's font, as represented by the "水" (CJK water ideograph, U+6C34) glyph.`,
+  lh: 'lh', // relative line height',
+
+  // --- viewport units --- //
+  dvh: 'dvh', // dynamic viewport height',
+  dvw: 'dvw', // dynamic viewport width',
+  lvh: 'lvh', // large viewport height',
+  lvw: 'lvw', // large viewport width',
+  svh: 'svh', // small viewport height',
+  svw: 'svw', // small viewport width',
+  vb: 'vb', // viewport size in the root element's block axis`,
+  vh: 'vh', // viewport height',
+  vi: 'vi', // viewport size in the root element's inline axis`,
+  vmax: 'vmax', // viewport larger dimension`,
+  vmin: 'vmin', // viewport smaller dimension`,
+  vw: 'vw', // viewport width',
+
+  // --- (query) container units --- //
+  cqb: 'cqb', // container block size',
+  cqh: 'cqh', // container height',
+  cqi: 'cqi', // container inline size',
+  cqmax: 'cqmax', // larger of `cqi` or `cqb`',
+  cqmin: 'cqmin', // smaller of `cqi` or `cqb`',
+  cqw: 'cqw', // container width',
+
+  // --- angle --- //
+  deg: 'deg', // degrees',
+  grad: 'grad', // gradians',
+  rad: 'rad', // radians',
+  turn: 'turn', // turns',
+
+  // --- time --- //
+  ms: 'ms', // milliseconds',
+  s: 's', // seconds',
+
+  // --- frequency --- //
+  Hz: 'Hz', // Hertz',
+  kHz: 'kHz', // KiloHertz',
+
+  // --- flex units --- //
+  fr: 'fr', // flexible length within grid container (*fr*action)',
+
+  // --- resolution --- //
+  dpcm: 'dpcm', // dots per centimeter',
+  dpi: 'dpi', // dots per inch',
+  dppx: 'dppx', // dots per px unit',
+  x: 'x', // (alias for `dppx`) dots per px unit',
+
+  // --- percentages --- //
+  '%': '%', // 'percentage',
+  pct: '%', // '`stylts` alias for `%`',
+} as const;
+
+export type UnitType = keyof typeof unitTypes;
+
+export type UnitValue = `${NumericValue}${UnitType}` | NumericValue;
+export type UnitValues = UnitValue[] | UnitValue[][];
+
+// Use the unitTypes object to create getters for each unit type
+
+export function unit(n: number | `${number}`) {
+  return new Proxy(unitTypes, {
+    get(_target, key) {
+      return n + unitTypes[key];
+    },
+  });
+}
+
+export function toUnit(value: UnitValues, unit: (UnitType | never)): string {
   const suffix = String(value) !== '0' ? unit : '';
   return arrayConcat(value).join(suffix + ' ') + suffix;
 }
 
 export const pxl = (...value: UnitValues) => toUnit(value, 'px');
-export const pts = (...value: UnitValues) => toUnit(value, 'pt');
-export const em = (...value: UnitValues) => toUnit(value, 'em');
+// local font-relative lengths
 export const rem = (...value: UnitValues) => toUnit(value, 'rem');
+export const em = (...value: UnitValues) => toUnit(value, 'em');
+export const ex = (...value: UnitValues) => toUnit(value, 'ex');
+export const lh = (...value: UnitValues) => toUnit(value, 'lh');
+export const pts = (...value: UnitValues) => toUnit(value, 'pt');
+// viewport units
 export const vh = (...value: UnitValues) => toUnit(value, 'vh');
 export const vw = (...value: UnitValues) => toUnit(value, 'vw');
-export const lh = (...value: UnitValues) => toUnit(value, 'lh');
 export const rlh = (...value: UnitValues) => toUnit(value, 'rlh');
 export const pct = (...value: UnitValues) => toUnit(value, '%');
+// rotation
+export const deg = (...value: UnitValues) => toUnit(value, 'deg');
+// time
+export const ms = (...value: UnitValues) => toUnit(value, 'ms');
+export const sec = (...value: UnitValues) => toUnit(value, 's');
 
 export class Unit {
-  value: UnitValues = [1];
-  num: UnitValue | UnitValues = 1;
-  n: number | string | `${number}${UnitType | ''}` | "" | UnitValue[] | UnitValue[][];
+  value: (UnitValues | NumericValue[]) = [1];
+  num: UnitValue | UnitValues | NumericValue = 1;
+  n: number | string | `${number}${UnitType | ''}` | '' | UnitValue[] | UnitValue[][];
 
   constructor(...value: UnitValues) {
     this.value = value;
@@ -111,30 +190,24 @@ export class Unit {
     return pct(...this.value);
   }
 
-  static $0 = new Unit(0);
-  static $100 = new Unit(100);
-  static full = Unit.$100;
+  static $0 = 0;
+  static none = 0;
+  static full = pct(100);
 }
 
-export function unit(...value: UnitValues) {
-  return new Unit(...value);
-}
-export const u = unit().u;
+// export function unit(...value: UnitValues) {
+//   return new Unit(...value);
+// }
 
 unit.px = pxl;
 unit.pt = pts;
 
-pxl.x0 = pxl(0);
-pxl.x5 = pxl(5);
-pxl.x10 = pxl(10);
-pxl.x20 = pxl(20);
-pxl.x40 = pxl(40);
-
 vh.full = vh(100);
 vw.full = vw(100);
-lh.x1 = lh(1);
-rlh.x1 = rlh(1);
-pct.full = pct.x100 = pct(100);
+lh.$1 = lh(1);
+rlh.$1 = rlh(1);
+pct.$100 = pct(100);
+pct.full = pct.$100
 
 function getLength(value: unknown): number {
   try {
@@ -163,10 +236,10 @@ function stripHex(value: NumericValue): string {
 //   return stripHex(value).length;
 // }
 
-function parseHex(value: NumericValue): [ string, number ] {
+function parseHex(value: NumericValue): [string, number] {
   const val = stripHex(value);
   const len = val.length;
-  return [ val, len ];
+  return [val, len];
 }
 
 // function isHexFull(value: NumericValue): boolean {
@@ -195,7 +268,7 @@ function parseHex(value: NumericValue): [ string, number ] {
  * @param value
  */
 export function toHex(value: NumericValue): string {
-  const [ val, len ] = parseHex(value);
+  const [val, len] = parseHex(value);
   switch (len) {
     case 6:
     case 3:
@@ -223,12 +296,12 @@ type RGBAValues = [RGBRange, RGBRange, RGBRange, number];
 // type AssertAlpha<Alpha extends number> = `${Alpha}` extends AlphaChanel ? Alpha : never;
 
 export function rgb(...rgb: RGBValues) {
-  const [ r, g, b ] = rgb;
+  const [r, g, b] = rgb;
   return `rgb(${r}, ${g}, ${b})`;
 }
 
 export function rgba(...rgba: RGBAValues) {
-  let [ r, g, b, a ] = rgba;
+  let [r, g, b, a] = rgba;
   if (a < 0) {
     a = 0;
   }
@@ -242,9 +315,9 @@ function parseHexString(value: NumericValue): string {
   let a: string | string[];
   let b: string | string[];
   let c: string | string[];
-  [ a ] = parseHex(value as NumericValue);
-  [ a, ...b ] = a.split('').slice(0, 2);
-  [ b, ...c ] = b.slice(0, 2);
+  [a] = parseHex(value as NumericValue);
+  [a, ...b] = a.split('').slice(0, 2);
+  [b, ...c] = b.slice(0, 2);
   return '';
 }
 
@@ -253,10 +326,10 @@ function hexToInt(value: NumericValue): number {
 }
 
 function hexToRGB(hexValue: NumericValue): number[] {
-  return [ 0 ];
+  return [0];
 }
 
-export function rgbToHex(value: NumericValue | [ NumericValue, NumericValue, NumericValue ]): string {
+export function rgbToHex(value: NumericValue | [NumericValue, NumericValue, NumericValue]): string {
   let rgb = '';
   if (Array.isArray(value)) {
     console.log(`I'm an array.`, value);
