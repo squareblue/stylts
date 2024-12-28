50fe33a1a3ed56f4ad189ff739d5d1b2683af30a
Mark M. Florida <4173034+mflorida@users.noreply.github.com>
2024-12-28 16:50:59 -0600

    Add aliases for 'padding' properties.

--- packages/stylts/src/properties/padding.ts
+++ packages/stylts/src/properties/padding.ts
@@ -12,47 +12,73 @@ export const padding = (value: number | number[] | Property.Padding) => ({
 export default padding;
 
 // top, bottom, left, right
-export const padTop = padding.t = padding.top = (value: number | Property.PaddingTop ) => ({
+export const paddingTop = (value: number | Property.PaddingTop) => ({
   paddingTop: numericStyleValue(value),
 });
-export const pt = padTop;
+export const paddingT = paddingTop;
+export const padTop = paddingTop;
+export const padT = paddingTop;
+export const pt = paddingTop;
+padding.t = paddingTop;
+padding.top = paddingTop;
 
-export const padBottom = padding.b = padding.bottom = (value: number | Property.PaddingBottom ) => ({
+export const paddingBotton = (value: number | Property.PaddingBottom) => ({
   paddingBottom: numericStyleValue(value),
 });
+export const paddingB = paddingBotton;
+export const padBottom = paddingBotton;
+export const padB = paddingBotton;
 export const pb = padBottom;
+padding.b = paddingBotton;
+padding.bottom = paddingBotton;
 
-export const padLeft = padding.l = padding.left = (value: number | Property.PaddingLeft ) => ({
+export const paddingLeft = (value: number | Property.PaddingLeft) => ({
   paddingLeft: numericStyleValue(value),
 });
-export const pl = padLeft;
+export const paddingL = paddingLeft;
+export const padLeft = paddingLeft;
+export const padL = paddingLeft;
+export const pl = paddingLeft;
+padding.l = paddingLeft;
+padding.left = paddingLeft;
 
-export const padRight = padding.r = padding.right = (value: number | Property.PaddingRight ) => ({
+export const paddingRight = (value: number | Property.PaddingRight) => ({
   paddingRight: numericStyleValue(value),
 });
-export const pr = padRight;
+export const paddingR = paddingRight;
+export const padRight = paddingRight;
+export const padR = paddingRight;
+export const pr = paddingRight;
+padding.r = paddingRight;
+padding.right = paddingRight;
 
 // x axis
-export const padX = padding.x = (value: number | Property.Padding) => ({
+export const paddingX = (value: number | Property.Padding) => ({
   paddingLeft: numericStyleValue(value),
   paddingRight: numericStyleValue(value),
 });
-export const px = padX;
+export const padX = paddingX;
+export const px = paddingX;
+padding.x = paddingX;
 
 // y axis
-export const padY = padding.y = (value: number | Property.Padding) => ({
+export const padY = (value: number | Property.Padding) => ({
   paddingTop: numericStyleValue(value),
   paddingBottom: numericStyleValue(value),
 });
-export const pY = padY;
+export const py = padY;
+padding.y = padY;
 
 // shortcuts
-export const p0 = padding.none = {
+export const p0 = {
   paddingTop: 0,
   paddingBottom: 0,
   paddingLeft: 0,
   paddingRight: 0,
 };
+
+padding.$0 = p0;
+padding.none = p0;
 
 export const p = padding;
 export const pad = padding;
